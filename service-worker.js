77fa54985c571a69d927df7318646649925da9ae
jan-martinek <honza.martinek@gmail.com>
2021-07-09 20:58:38 +0000

    Deploying to docs from @ books-are-next/bila-nemoc@5954eec252c4fe5df30bad1e6468c8283cfb676a 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-08df0bc';
+const CACHE = 'cache-5954eec';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./druhe-1.html","./druhe-2.html","./druhe-3.html","./druhe-4.html","./druhe-5.html","./druhe-6.html","./druhe.html","./favicon.png","./index.html","./manifest.json","./osoby.html","./predmluva.html","./prvni-1.html","./prvni-2.html","./prvni-4.html","./prvni-5.html","./prvni-3.html","./prvni.html","./treti-1.html","./treti-2.html","./treti-3.html","./zivot.html","./treti.html","./images/jednani_druhe.mp4","./images/jednani_prvni.mp4","./images/jednani_treti.mp4","./images/titulni.mp4","./scripts/bundle.js","./style/style.min.css","./fonts/spectral/SIL Open Font License.txt","./fonts/spectral/Spectral-Bold.woff2","./fonts/spectral/Spectral-BoldItalic.woff2","./fonts/spectral/Spectral-Medium.woff2","./fonts/spectral/Spectral-MediumItalic.woff2","./fonts/space/AUTHORS.txt","./fonts/space/CONTRIBUTORS.txt","./fonts/space/OFL.txt","./fonts/space/webfont/SpaceGrotesk-Bold.woff","./fonts/space/webfont/SpaceGrotesk-Bold.woff2","./fonts/space/webfont/SpaceGrotesk-Light.woff","./fonts/space/webfont/SpaceGrotesk-Light.woff2","./fonts/space/webfont/SpaceGrotesk-Medium.woff","./fonts/space/webfont/SpaceGrotesk-Medium.woff2","./fonts/space/webfont/SpaceGrotesk-Regular.woff","./fonts/space/webfont/SpaceGrotesk-Regular.woff2","./fonts/space/webfont/SpaceGrotesk-SemiBold.woff","./fonts/space/webfont/SpaceGrotesk-SemiBold.woff2","./fonts/space/variable/SpaceGroteskVariable.woff2","./fonts/space/variable/SpaceGroteskVariable.ttf"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./druhe-1.html","./druhe-2.html","./druhe-3.html","./druhe-4.html","./druhe-5.html","./druhe-6.html","./druhe.html","./favicon.png","./index.html","./manifest.json","./osoby.html","./predmluva.html","./prvni-1.html","./prvni-2.html","./prvni-3.html","./prvni-4.html","./prvni-5.html","./prvni.html","./treti-1.html","./treti-2.html","./treti-3.html","./treti.html","./zivot.html","./images/jednani_druhe.mp4","./images/jednani_prvni.mp4","./images/jednani_treti.mp4","./images/titulni.mp4","./scripts/bundle.js","./style/style.min.css","./fonts/spectral/SIL Open Font License.txt","./fonts/spectral/Spectral-Bold.woff2","./fonts/spectral/Spectral-BoldItalic.woff2","./fonts/spectral/Spectral-Medium.woff2","./fonts/spectral/Spectral-MediumItalic.woff2","./fonts/space/AUTHORS.txt","./fonts/space/CONTRIBUTORS.txt","./fonts/space/OFL.txt","./fonts/space/variable/SpaceGroteskVariable.ttf","./fonts/space/variable/SpaceGroteskVariable.woff2","./fonts/space/webfont/SpaceGrotesk-Bold.woff","./fonts/space/webfont/SpaceGrotesk-Bold.woff2","./fonts/space/webfont/SpaceGrotesk-Light.woff","./fonts/space/webfont/SpaceGrotesk-Light.woff2","./fonts/space/webfont/SpaceGrotesk-Medium.woff","./fonts/space/webfont/SpaceGrotesk-Medium.woff2","./fonts/space/webfont/SpaceGrotesk-Regular.woff","./fonts/space/webfont/SpaceGrotesk-Regular.woff2","./fonts/space/webfont/SpaceGrotesk-SemiBold.woff","./fonts/space/webfont/SpaceGrotesk-SemiBold.woff2"]));
 }
 
 self.addEventListener('fetch', e => {
